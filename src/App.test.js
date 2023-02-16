e87169c76066698719d476ae7ae6b6f1d2ac858a
Gerson Herrera <gerson.19070034@gmail.edu.mx>
2023-02-16 06:02:36 -0600

    Ultimos comentarios

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react'; //Se importan las librerias que vamos a usar
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link', () => { //ejecuta el juego por medio de react
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
